7b429888af6d812bd6580f78a807dc5493470644
iamqjin <iamqjin@gmail.com>
2017-01-26 17:07:31 +0900

    미미 ver1.0 prototype

--- ImagePickerExperiment/ViewController.swift
+++ ImagePickerExperiment/ViewController.swift
@@ -8,18 +8,195 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate ,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    //미미 구조체 선언부
+    struct Meme {
+        let topText: String?
+        let bottomText: String?
+        let originalImage: UIImage?
+        let memedImage: UIImage?
+    }
+    
+    
+    //MARK: 아울렛 선언
+    @IBOutlet weak var imagePickView: UIImageView!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var firstTextField: UITextField!
+    @IBOutlet weak var secondTextField: UITextField!
 
+    @IBOutlet weak var topToolbar: UIToolbar!
+    
+    @IBOutlet weak var bottomToolbar: UIToolbar!
+    
+    @IBAction func shareImage(_ sender: Any) {
+        
+        let meme = Meme(topText: firstTextField.text!, bottomText: secondTextField.text!, originalImage: imagePickView.image!, memedImage: generateMemedImage())
+
+        print(meme)
+        let activityViewController = UIActivityViewController(activityItems: [meme.memedImage!], applicationActivities: nil)
+        
+        self.present(activityViewController, animated: true, completion: nil)
+        
+    }
+    
+    //MARK: 액션 선언
+    @IBAction func pickAnImageFromAlbum(_ sender: Any) {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = self
+        
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func pickAnImageFromCamera(_ sender: Any) {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        shareButton.isEnabled = false
+        firstTextField.delegate = self
+        secondTextField.delegate = self
+        
+        let memeTextAttributes:[String:Any] = [
+            NSStrokeColorAttributeName: UIColor.black,
+            NSForegroundColorAttributeName: UIColor.white,
+            NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSStrokeWidthAttributeName: -2.0]
+
+        firstTextField.text = "TOP"
+        secondTextField.text = "BOTTOM"
+        firstTextField.defaultTextAttributes = memeTextAttributes
+        firstTextField.textAlignment = NSTextAlignment.center
+        
+        secondTextField.defaultTextAttributes = memeTextAttributes
+        secondTextField.textAlignment = NSTextAlignment.center
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //MARK: 메소드 선언
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        
+        picker.dismiss(animated: false) { () in
+        
+        let alert = UIAlertController(title: "", message: "이미지 선택이 취소되었습니다.", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "확인", style: .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+        
+        }
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        picker.dismiss(animated: true) { () in
+            
+            if let img = info[UIImagePickerControllerEditedImage] as? UIImage{
+                self.imagePickView.image = img
+            }
+            //이미지 저장 후 공유버튼 활성화
+            self.shareButton.isEnabled = true
+        }
+        
+    }
+    
+    
+    //텍스트 필드 델리게이트 메소드 선언부
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.text = ""
+    }
+    
+    //return 입력시 키보드 사라짐
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.endEditing(true)
+        return true
+    }
+    
+    func keyboardWillShow(_ notification:Notification) {
+        
+        view.frame.origin.y = 0 - getKeyboardHeight(notification)
+    }
+    
+    func keyboardWillHide(_ notification:Notification){
+        
+        view.frame.origin.y = 0
+    }
+    
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+    
+    func subscribeToKeyboardNotifications() {
+        //수정되어야할 부분 버전업
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
+        
     }
 
+    //미미 구조체에 저장 부분
+//    func save() {
+//        // Create the meme
+//        let meme = Meme(topText: firstTextField.text!, bottomText: secondTextField.text!, originalImage: imagePickView.image!, memedImage: generateMemedImage())
+//        
+//    }
+    
+    //미미 수정된 이미지 생성 부분
+    func generateMemedImage() -> UIImage {
+        
+        // TODO: Hide toolbar and navbar
+        self.topToolbar.isHidden = true
+        self.bottomToolbar.isHidden = true
+        
+        print("나호출됬따!!!")
+        // Render view to an image
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let memedImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        // TODO: Show toolbar and navbar
+        self.topToolbar.isHidden = false
+        self.bottomToolbar.isHidden = false
+        
+        return memedImage
+    }
 
 }
 
